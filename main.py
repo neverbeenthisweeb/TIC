aaba2abb961124350abcbf367fbb0b2a6b3ce424
Rafi Ramadhana <hf@ip-192-168-100-4.ap-southeast-1.compute.internal>
2019-12-21 06:56:07 +0700

    Add get/ download mechanism for images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import shutil
+import requests
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -8,13 +10,14 @@ SCROLL_TIMES = 5
 
 if __name__=="__main__":
 	current_scroll_time = 0
+	img_counter = 0
 	driver = webdriver.Chrome()
 	driver.get(url=URL)
 
-	while current_scroll_time < SCROLL_TIMES:
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		current_scroll_time+=1
-		time.sleep(2)
+	# while current_scroll_time < SCROLL_TIMES:
+	# 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	# 	current_scroll_time+=1
+	# 	time.sleep(2)
 
 
 	source = driver.page_source
@@ -23,6 +26,14 @@ if __name__=="__main__":
 	images = soup.find_all("div", class_="AdaptiveMedia-photoContainer")
 
 	for img in images:
-		print(img['data-image-url'])
+		img_url = img['data-image-url']
+		filename = img_url.split("/")[-1]
+		response = requests.get(img_url, stream=True)
+		with open(f"DownloadedImages/{filename}", 'wb') as outfile:
+			shutil.copyfileobj(response.raw, outfile)
+		print(f"[{img_counter}] Downloaded: {img_url}")
+		img_counter+=1
+		del response
+
 
 	driver.quit()
